70a0c911f9931bce86f6640b018c753c58748b9e
Dmitry Karapata <karapata@gmail.com>
2022-08-08 13:19:08 +0300

    add sum

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,3 +1,6 @@
-﻿int numberA = 31;
-int NumberB = 5;
-Console.WriteLine(numberA + NumberB);
+﻿int numberA = new Random() .Next(1,10); //1 2 3 4 ..... 9
+Console.WriteLine(numberA);
+int NumberB = new Random() .Next(1,10);
+Console.WriteLine(NumberB);
+int result = numberA + NumberB;
+Console.WriteLine(result);
